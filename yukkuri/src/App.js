4fc6afca3103c973c00196682a2bdf7d0faca482
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-08 23:23:43 +0900

    add form

--- yukkuri/src/App.js
+++ yukkuri/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>yukkuri</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="text" placeholder="Type text here"/>
+          <button onClick={()=>{}}>analyze</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <text>ここに結果</text>
       </header>
     </div>
   );
