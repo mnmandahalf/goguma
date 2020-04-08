8e01ac016c4cafdf7d14e1b1a0cbf52a63bd64c9
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-08 23:30:18 +0900

    devide components

--- yukkuri/src/App.js
+++ yukkuri/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Form } from './Form.js';
+import { AnalysisResultArea } from './AnalysisResultArea.js';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>yukkuri</h1>
-        <p>
-          <input type="text" placeholder="Type text here"/>
-          <button onClick={()=>{}}>analyze</button>
-        </p>
-        <text>ここに結果</text>
+        <Form />
+        <AnalysisResultArea />
       </header>
     </div>
   );
---
+++ yukkuri/src/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Form = () => {
+  return (
+    <p>
+      <input type="text" placeholder="Type text here"/>
+      <button onClick={()=>{}}>analyze</button>
+    </p>
+  )
+}
