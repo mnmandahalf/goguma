d78816827efb06536aa510c7089da46eb7684cf4
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-09 02:10:19 +0900

    add logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import logo from '../logo.png';
 import { Form } from './Form.js';
 import { AnalysisResultArea } from './AnalysisResultArea.js';
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>yukkuri</h1>
         <Form />
         <AnalysisResultArea />
