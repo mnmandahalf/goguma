baac37c4f814d221553b85bf06ef55d4662261d8
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-08 23:39:07 +0900

    add form event

--- yukkuri/src/Form.js
+++ yukkuri/src/Form.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Form = () => {
+  const [inputText, setInputText] = useState('')
+  const handleChange = (event) => setInputText(event.target.value)
   return (
     <p>
-      <input type="text" placeholder="Type text here"/>
-      <button onClick={()=>{}}>analyze</button>
+      <input type="text"
+        placeholder="テキストを入力"
+        onChange={handleChange}
+        value={inputText}
+      />
+      <button onClick={()=>{console.log(inputText)}}>解析する</button>
     </p>
   )
 }
