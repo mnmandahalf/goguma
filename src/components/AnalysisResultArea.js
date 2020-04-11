02e5d1712ce96be39144717ba40355ab5de058a0
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-12 05:57:58 +0900

    トークンのリセットが機能するよう修正

--- src/components/AnalysisResultArea.js
+++ src/components/AnalysisResultArea.js
@@ -4,15 +4,6 @@ import classes from './AnalysisResultArea.module.css'
 import { Card } from '@material-ui/core'
 import { Tokens } from './Tokens'
 
-const sampleTokens = [
-  {token: "오늘", romanized: "oneul", word_class: "一般名詞", translation: "今日では"},
-  {token: "은", romanized: "eun", word_class: "終助詞", translation: "は"},
-  {token: "좋", romanized: "jo", word_class: "形容詞", translation: "よく"},
-  {token: "은", romanized: "eun", word_class: "語尾", translation: "は"},
-  {token: "날씨", romanized: "nalssi", word_class: "一般名詞", translation: "天候"},
-  {token: "이", romanized: "i", word_class: "格助詞（が）", translation: "これは"},
-  {token: "네요", romanized: "neyo", word_class: "語尾", translation: "ね"}
-]
 export const AnalysisResultArea = () => {
   const result = useSelector(state => state.analysis)
   return (
@@ -30,7 +21,7 @@ export const AnalysisResultArea = () => {
       }
       <span>品詞分解</span>
       <p>
-        <Tokens tokens={result.tokens.length === 0 ? sampleTokens : result.tokens} />
+        <Tokens tokens={result.tokens} />
       </p>
     </Card>
   )
--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { Card } from '@material-ui/core';
 import classes from './Tokens.module.css'
 
-export const Tokens = ({ tokens }) => (
- <div className={classes.tokens}>
-  {
-    tokens.map(item =>
-      <Card variant="outlined" key={item.token} className={classes.tokenBlock}>
-        <div className={classes.tokenSmall}>{item.romanized}</div>
-        <div>{item.token}</div>
-        <div className={classes.tokenSmall}>{item.word_class}</div>
-        <div className={classes.tokenSmall}>{item.translation}</div>
-      </Card>)
-  }
- </div>
-)
+const sampleTokens = [
+  {token: "오늘", romanized: "oneul", word_class: "一般名詞", translation: "今日では"},
+  {token: "은", romanized: "eun", word_class: "終助詞", translation: "は"},
+  {token: "좋", romanized: "jo", word_class: "形容詞", translation: "よく"},
+  {token: "은", romanized: "eun", word_class: "語尾", translation: "は"},
+  {token: "날씨", romanized: "nalssi", word_class: "一般名詞", translation: "天候"},
+  {token: "이", romanized: "i", word_class: "格助詞（が）", translation: "これは"},
+  {token: "네요", romanized: "neyo", word_class: "語尾", translation: "ね"}
+]
+
+export const Tokens = ({ tokens }) => {
+  const items = tokens.length > 0 ? tokens : sampleTokens
+  return (
+    <div className={classes.tokens}>
+      {
+        items.map((item, index) =>
+          <Card variant="outlined" key={item.token + index} className={classes.tokenBlock}>
+            <div className={classes.tokenSmall}>{item.romanized}</div>
+            <div>{item.token}</div>
+            <div className={classes.tokenSmall}>{item.word_class}</div>
+            <div className={classes.tokenSmall}>{item.translation}</div>
+          </Card>)
+      }
+    </div>
+  )
+}
