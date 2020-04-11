1179725a0d87e2ec606acc3d2a86e6ca08046c5a
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-12 04:33:52 +0900

    レイアウト調整

--- src/components/AnalysisResultArea.js
+++ src/components/AnalysisResultArea.js
@@ -6,8 +6,8 @@ const sampleTokens = [
   {token: "오늘", romanized: "oneul", word_class: "一般名詞", translation: "今日では"},
   {token: "은", romanized: "eun", word_class: "終助詞", translation: "は"},
   {token: "좋", romanized: "jo", word_class: "形容詞", translation: "よく"},
-  {token: "은", romanized: "nalssi", word_class: "語尾", translation: "は"},
-  {token: "날씨", romanized: "oneul", word_class: "一般名詞", translation: "天候"},
+  {token: "은", romanized: "eun", word_class: "語尾", translation: "は"},
+  {token: "날씨", romanized: "nalssi", word_class: "一般名詞", translation: "天候"},
   {token: "이", romanized: "i", word_class: "格助詞（が）", translation: "これは"},
   {token: "네요", romanized: "neyo", word_class: "語尾", translation: "ね"}
 ]
@@ -27,27 +27,29 @@ export const AnalysisResultArea = () => {
           <p>{result.romanized}</p>
       }
       <span>品詞分解</span>
-      <div className={classes.tokens}>
-        {
-          result.tokens.length === 0 ?
-          (sampleTokens.map(item =>
-            <Card variant="outlined" key={item.token} className={`${classes.tokenBlock} ${classes.example}`}>
-              <div className={classes.tokenSmall}>{item.romanized}</div>
-              <div className={classes.tokenItem}>{item.token}</div>
-              <div className={classes.tokenSmall}>{item.word_class}</div>
-              <div className={classes.tokenSmall}>{item.translation}</div>
-            </Card>)
-          ) :
-          (result.tokens.map(item =>
-            <Card variant="outlined" key={item.token} className={classes.tokenBlock}>
-              <div className={classes.tokenSmall}>{item.romanized}</div>
-              <div className={classes.tokenItem}>{item.token}</div>
-              <div className={classes.tokenSmall}>{item.word_class}</div>
-              <div className={classes.tokenSmall}>{item.translation}</div>
-            </Card>)
-          )
-        }
-      </div>
+      <p>
+        <div className={classes.tokens}>
+          {
+            result.tokens.length === 0 ?
+            (sampleTokens.map(item =>
+              <Card variant="outlined" key={item.token} className={`${classes.tokenBlock} ${classes.example}`}>
+                <div className={classes.tokenSmall}>{item.romanized}</div>
+                <div className={classes.tokenItem}>{item.token}</div>
+                <div className={classes.tokenSmall}>{item.word_class}</div>
+                <div className={classes.tokenSmall}>{item.translation}</div>
+              </Card>)
+            ) :
+            (result.tokens.map(item =>
+              <Card variant="outlined" key={item.token} className={classes.tokenBlock}>
+                <div className={classes.tokenSmall}>{item.romanized}</div>
+                <div className={classes.tokenItem}>{item.token}</div>
+                <div className={classes.tokenSmall}>{item.word_class}</div>
+                <div className={classes.tokenSmall}>{item.translation}</div>
+              </Card>)
+            )
+          }
+        </div>
+      </p>
     </Card>
   )
 }
