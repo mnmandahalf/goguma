38aaf33d011d6cff6116a325021d90b1368319c2
Manami Nakamura <manaminakamura812@gmail.com>
2020-04-11 17:43:24 +0900

    fix

--- src/components/AnalysisResultArea.js
+++ src/components/AnalysisResultArea.js
@@ -5,17 +5,17 @@ export const AnalysisResultArea = () => {
   const result = useSelector(state => state.analysis)
   return (
     <p>
-      <span>結果：</span>
+      <span>- 結果 -</span>
       <p>{result.text}</p>
       <p>{result.romanized}</p>
       <p>{result.translation}</p>
       {
         result.tokens.map(item =>
           <div key={item.token}>
-            <p>{item.token}</p>
-            <p>{item.romanized}</p>
-            <p>{item.translation}</p>
-            <p>{item.translation}</p>
+            <span>{item.token}</span>
+            <span>{item.romanized}</span>
+            <span>{item.translation}</span>
+            <span>{item.word_class}</span>
           </div>
         )
       }
