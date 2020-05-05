1afcd647ea86514f1e043238d7079ef80454042a
Manami Nakamura <manaminakamura812@gmail.com>
2020-05-05 16:33:24 +0900

    change host

--- src/epics/index.js
+++ src/epics/index.js
@@ -7,7 +7,7 @@ import { requestObservable } from './request'
 const fetchAnalysisEpic = action$ => action$.pipe(
   ofType('FETCH_ANALYSIS'),
   requestObservable(
-    action => ajax.getJSON(`https://korean-analyze-api.herokuapp.com//analyze?text=${action.text}`),
+    action => ajax.getJSON(`http://35.211.129.214:8000/analysis?text=${action.text}`),
     response => fullfilledAnalysis(response)
   )
 );
