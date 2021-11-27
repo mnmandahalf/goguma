cb94098779518d0737bc06d7972d5a4f020325e0
manaminakamura <manaminakamura812@gmail.com>
2021-11-28 02:19:40 +0900

    ログイン状態の出し分け

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { SwipeableDrawer, List, ListItem, Link } from '@material-ui/core';
+import React, { useState } from 'react'
+import { SwipeableDrawer, List, ListItem, Link, Typography } from '@material-ui/core';
+import { auth } from "../firebase.js";
 
 export const Drawer = ({ open, onClose, onOpen}) => {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [authUser, setAuthUser] = useState()
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setIsSignedIn(true)
+      setAuthUser(user)
+    } else {
+      setIsSignedIn(false)
+    }
+  });
   return (
     <SwipeableDrawer
       open={open}
@@ -10,15 +21,21 @@ export const Drawer = ({ open, onClose, onOpen}) => {
     >
       <div style={{width: 300}}>
         <List>
+          {
+            isSignedIn &&
+            <ListItem>
+              <Typography variant="h6">{authUser && authUser.email}さん</Typography>
+            </ListItem>
+          }
           <ListItem>
             <Link href="/">ホーム</Link>
           </ListItem>
-          <ListItem>
-            <Link href="/sign_up">ユーザー登録</Link>
-          </ListItem>
-          <ListItem>
-            <Link href="/sign_in">ログイン</Link>
-          </ListItem>
+          {
+            !isSignedIn &&
+            <ListItem>
+              <Link href="/sign_in">ログイン</Link>
+            </ListItem>
+          }
           <List>
             <ListItem>
               <Link href="http://mnmandahalf.hatenablog.com/">お問い合わせは管理人ブログへ</Link>
