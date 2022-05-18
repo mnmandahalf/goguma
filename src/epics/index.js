355ca891c83c5b3ed9384115268a95d02f4e7dac
manaminakamura <manaminakamura812@gmail.com>
2022-05-19 02:29:09 +0900

    change api url

--- src/epics/index.js
+++ src/epics/index.js
@@ -6,7 +6,7 @@ import { requestObservable } from './request'
 const fetchAnalysisEpic = action$ => action$.pipe(
   ofType('FETCH_ANALYSIS'),
   requestObservable(
-    action => ajax.getJSON(`https://yukkuri-konlpy-api.com/analysis?text=${action.text}`),
+    action => ajax.getJSON(`https://django-konlpy-api-wbc37rju4a-uc.a.run.app/analysis?text=${action.text}`),
     response => fullfilledAnalysis(response)
   )
 );
